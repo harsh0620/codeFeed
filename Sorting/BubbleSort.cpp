05bb5556666698a292f0d9e2b8a9e0779e6cc365
harsh0620 <harsh0111chandravanshi@gmail.com>
2021-08-06 09:43:03 +0530

    Bubble-Sort+Recursive-Bubble-sort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -17,6 +17,24 @@ void swap(int *xp, int *yp)
     *yp = temp;
 }
 
+void recurBubbleSort(int arr[], int n)
+{
+    // Base case
+    if (n == 1)
+        return;
+
+    // One pass of bubble sort. After
+    // this pass, the largest element
+    // is moved (or bubbled) to end.
+    for (int i = 0; i < n - 1; i++)
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+
+    // Largest element is fixed,
+    // recur for remaining array
+    bubbleSort(arr, n - 1);
+}
+
 void bubbleSort(int a[], int n)
 {
     for (int i = 0; i < n - 1; i++)
