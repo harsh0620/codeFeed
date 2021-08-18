a9b2bed568e03bc60b6b40b10ffdb90e9ea32224
harsh0620 <harsh0111chandravanshi@gmail.com>
2021-08-18 18:06:41 +0530

    DLLInsertion

---
+++ DataStructure/LinkedList/Doubly_Linked_List/DLLInsertion.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        next = prev = NULL;
+    }
+};
+node *insertBegin(node *head, int x)
+{
+    node *temp = new node(x);
+    temp->next = head;
+    if (head != NULL)
+    {
+        head->prev = temp;
+    }
+    return temp;
+}
+node *insertAtEnd(node *head, int x)
+{
+    node *temp = new node(x);
+    node *curr = head;
+    if (head == NULL)
+    {
+        return temp;
+    }
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    temp->prev = curr;
+    return head;
+}
+void displayLL(node *head)
+{
+    if (head == NULL)
+    {
+        cout << "UNDERFLOW" << endl;
+        return;
+    }
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    node *head = NULL;
+    head = insertBegin(head, 40);
+    head = insertBegin(head, 30);
+    head = insertBegin(head, 20);
+    head = insertBegin(head, 10);
+    head = insertAtEnd(head, 10);
+
+    displayLL(head);
+
+    return 0;
+}
